7bc42ceddcf6db18c13c92d9ab207393a3dc94d9
Nour_B <nour-el-imene.benmoussa@etu.unice.fr>
2017-09-20 14:48:46 +0200

    Ajout de POIs via Bootstrap + mise en page page d'accueil

--- grails-app/init/grails_pointofinterests/BootStrap.groovy
+++ grails-app/init/grails_pointofinterests/BootStrap.groovy
@@ -18,7 +18,19 @@ class BootStrap {
         UserRole.create(administrateurUser, administrateurRole, true);
         UserRole.create(moderateurUser, moderateurRole, true);
         UserRole.create(utilisateurUser, utilisateurRole, true);
+
+        (1..3).each {
+            int i ->
+                def groupe = new Groupe(name: 'groupe ' + i, description: 'description ' + i).save(flush: true, failOnError: true)
+
+                (1..5).each {
+                    int j ->
+                        def point = new Point(name: "POI " + j, description: 'description ' + j).save(flush: true, failOnError: true)
+                        def groupepoi = new GroupePoi(groupe: groupe, point:point ).save(flush: true, failOnError: true)
+                }
+        }
     }
+
     def destroy = {
     }
 }
