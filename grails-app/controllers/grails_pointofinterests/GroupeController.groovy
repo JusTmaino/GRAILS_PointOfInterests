cd9f7adeedd776a1e6fbe48b3cc6ad150d11f252
Amine-PC\Amine <amine.elleuch1@gmail.com>
2017-09-24 01:11:44 +0200

    Create UI of Groupe / Point / Role

--- grails-app/controllers/grails_pointofinterests/GroupeController.groovy
+++ grails-app/controllers/grails_pointofinterests/GroupeController.groovy
@@ -10,7 +10,9 @@ class GroupeController {
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond Groupe.list(params), model:[groupeCount: Groupe.count()]
+        List<Groupe> GroupesList = Groupe.findAll()
+        [customGroupesList:GroupesList]
+        //respond Groupe.list(params), model:[groupeCount: Groupe.count()]
     }
 
     def show(Groupe groupe) {
--- grails-app/controllers/grails_pointofinterests/PointController.groovy
+++ grails-app/controllers/grails_pointofinterests/PointController.groovy
@@ -10,7 +10,9 @@ class PointController {
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond Point.list(params), model:[pointCount: Point.count()]
+        List<Point> PointsList = Point.findAll()
+        [customPointsList:PointsList]
+        //respond Point.list(params), model:[pointCount: Point.count()]
     }
 
     def show(Point point) {
